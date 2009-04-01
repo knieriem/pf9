6175e5bcd224eb0d6a4d896ba5fd7b401f981f63
Michael Teichgräber <mt@ib.wmipf.de>
2009-04-02 00:17:59 +0200

    update devdraw.h

--- src/cmd/devdraw/devdraw.h
+++ src/cmd/devdraw/devdraw.h
@@ -2,6 +2,6 @@ int _drawmsgread(void*, int);
 int _drawmsgwrite(void*, int);
 void _initdisplaymemimage(Memimage*);
 int _latin1(Rune*, int);
+int parsewinsize(char*, Rectangle*, int*);
+int mouseswap(int);
 
-struct Wsysmsg;
-void	runmsg(struct Wsysmsg*);
