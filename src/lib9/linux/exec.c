146fadf3699fd8d34003dade72fb5591fbacdf84
Michael Teichgräber <mt@ib.wmipf.de>
2009-04-18 13:15:43 +0200

    src/lib9/linux: remove files that don't differ from those in $PLAN9/src/lib9

--- src/lib9/linux/exec.c
+++
@@ -1,9 +0,0 @@
-#include <u.h>
-#include <libc.h>
-
-int
-exec(char *prog, char *argv[])
-{
-	/* to mimic plan 9 should be just exec, but execvp is a better fit for unix */
-	return execvp(prog, argv);
-}
